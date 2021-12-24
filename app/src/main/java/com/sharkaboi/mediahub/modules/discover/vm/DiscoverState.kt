fc7a4a275577cace05365e2845a4d6867c0d3233
Sarath S <sarathsedu@gmail.com>
2021-12-24 11:35:12 +0530

    Refactor to savedStateHandle to handle FragmentStateManager recreating view causing reload (7)

--- app/src/main/java/com/sharkaboi/mediahub/modules/discover/vm/DiscoverState.kt
+++ app/src/main/java/com/sharkaboi/mediahub/modules/discover/vm/DiscoverState.kt
@@ -1,12 +1,10 @@
 package com.sharkaboi.mediahub.modules.discover.vm
 
 import androidx.lifecycle.MutableLiveData
-import com.sharkaboi.mediahub.modules.discover.util.DiscoverAnimeListWrapper
 
 sealed class DiscoverState {
     object Idle : DiscoverState()
     object Loading : DiscoverState()
-    data class AnimeDetailsSuccess(val data: DiscoverAnimeListWrapper) : DiscoverState()
     data class AnimeDetailsFailure(val message: String) : DiscoverState()
 }
 
@@ -17,11 +15,6 @@ fun MutableLiveData<DiscoverState>.setLoading() = this.apply {
 fun MutableLiveData<DiscoverState>.setIdle() = this.apply {
     value = DiscoverState.Idle
 }
-
-fun MutableLiveData<DiscoverState>.setFetchSuccess(data: DiscoverAnimeListWrapper) =
-    this.apply {
-        value = DiscoverState.AnimeDetailsSuccess(data)
-    }
 
 fun MutableLiveData<DiscoverState>.setFailure(message: String) = this.apply {
     value = DiscoverState.AnimeDetailsFailure(message)
